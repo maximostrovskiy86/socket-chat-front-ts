75b592900e5b3a17bda9b69c005bab528daf1df3
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2025-09-09 15:25:10 +0200

    chore:setting up and organizing the project

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
 import store from "./redux/store";
 import "./index.css"
-
-
 import './index.css'
 import App from './components/app/App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store.store}>
         <StrictMode>
             <PersistGate loading={null} persistor={store.persistor}>
